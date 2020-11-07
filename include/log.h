506507ada087024a54a6b670510a1dc5b21fa278
UlaShu <60555614+UlaShu@users.noreply.github.com>
2020-11-07 09:50:11 +0300

    Update log.h

--- include/log.h
+++ include/log.h
@@ -1,1 +1,14 @@
+#ifndef __LOG_H
+#define _LOG_H
 
+// Длина буфера для Live logs
+#define LIVE_LOG_BUFFER_LEN 128
+
+// Тэги для логирования
+typedef enum {
+  TAG_GPS,
+}log_tag_num_t;
+
+extern char* log_name(log_tag_num_t tag);
+
+#endif
