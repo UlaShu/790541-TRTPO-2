acc46431830b06bc6ee557263ae10d1a398579a2
UlaShu <60555614+UlaShu@users.noreply.github.com>
2020-11-07 10:38:30 +0300

    Update log.h

--- include/log.h
+++ include/log.h
@@ -9,6 +9,12 @@ typedef enum {
   TAG_GPS,
 }log_tag_num_t;
 
+typedef struct {
+	char* tag;
+	esp_log_level_t level;
+}log_tag_t;
+
+// Имена тегов и уровень логирования
 extern char* log_name(log_tag_num_t tag);
 
 #endif
